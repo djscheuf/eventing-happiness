8eab4319bb2609a2ab0d21d89d8724ed062d297a
Daniel Scheufler <djscheufler@gmail.com>
2024-06-05 20:53:26 -0500

    wip(table-based current state)

---
+++ compute/func/current-state-db.spec.ts
@@ -0,0 +1,91 @@
+import { expect, test, describe, beforeEach } from 'vitest'
+import { Account } from '../models/account';
+import { Database } from '../models/db';
+import { Transaction } from '../models/transaction';
+import { currentStateOf } from './current-state-db'
+
+
+
+describe("Current State Of Account",()=>{
+    let mockDb: Database;
+    beforeEach(()=>{
+        mockDb = {
+            accounts:[],
+            transactions:[],
+        }
+    });
+    
+    describe('Given Some Accounts',()=>{
+        let aliceAcct: Account;
+        let systemAcct: Account;
+        beforeEach(()=>{
+            systemAcct = {
+                id: 0,
+                name: "system"
+            };
+            mockDb.accounts.push(systemAcct);
+
+            aliceAcct = {
+                id: 1,
+                name: "alice"
+            };
+            mockDb.accounts.push(aliceAcct);
+        });
+        describe('Given an init Transactions',()=>{
+            const transactions: Transaction[] = [];
+            let lastTransaction: number = 0;
+            let startBal: number = 0;
+            beforeEach(()=>{
+                startBal = 10;
+                mockDb.transactions.push({
+                    when:lastTransaction,
+                    transmitter:0,
+                    receiver:1,
+                    amount: startBal,
+                });
+            });
+            describe('When get current account state for Alice',()=>{
+                let result;
+                beforeEach(()=>{
+                        result = currentStateOf(aliceAcct.id,mockDb);
+                });
+                test('Then state only shows initial value',()=>{
+                        expect(result).toBe(mockDb.transactions[0].amount);
+                });
+            });
+            describe('Given some Debits',()=>{
+                let debits: number = 0;
+                beforeEach(()=>{
+                    lastTransaction++;
+                    let amt = 1;
+                    debits +=amt;
+                    mockDb.transactions.push({
+                        when:lastTransaction,
+                        transmitter:1,
+                        receiver:0,
+                        amount: amt,
+                    });
+
+                    lastTransaction++;
+                    amt = 3;
+                    debits += amt;
+                    mockDb.transactions.push({
+                        when:lastTransaction,
+                        transmitter:1,
+                        receiver:0,
+                        amount: amt,
+                    });
+                });
+                describe('When get current account state for Alice',()=>{
+                    let result;
+                    beforeEach(()=>{
+                        result = currentStateOf(aliceAcct.id, mockDb);
+                    });
+                    test('Then state shows current value',()=>{
+                        expect(result).toBe(startBal-debits);
+                    });
+                });
+            });
+        });
+    });    
+})
--- compute/models/account.ts
+++ compute/models/account.ts
@@ -2,4 +2,5 @@ export type Account = {
   // id: UUID
   id: number;
   name: string;
+  balance?: number;
 };
